3158ac6c07201ab83a7f75892aa8e702a457b6f8
1111 <1111@hb5011>
2016-09-13 11:24:02 +0900

    standalone

---
+++ src/main/java/com/hanbit/dongsu/seo/standalone/Animal.java
@@ -0,0 +1,6 @@
+package com.hanbit.dongsu.seo.standalone;
+
+public interface Animal {
+
+	static final int legs = 3;
+}
--- src/main/java/com/hanbit/dongsu/standalone/dongsu.java
+++
@@ -1,7 +0,0 @@
-package com.hanbit.dongsu.standalone;
-
-public class dongsu {
-public static void main(String[] args) {
-	System.out.println("hi");
-}
-}
